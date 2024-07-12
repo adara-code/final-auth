bacd84894ac48c2fc298343b14cff441083150da
adara-code <wakariuki.agnes@gmail.com>
2024-07-12 12:19:08 +0300

    Removed rge dotenv package

--- authentication/settings.py
+++ authentication/settings.py
@@ -2,8 +2,6 @@ from pathlib import Path
 from decouple import config
 from datetime import timedelta
 import os
-from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
